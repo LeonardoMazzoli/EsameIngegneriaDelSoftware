9425d3e02ae1e3c7f34759463fe2467fe3c2f6d7
LeonardoMazzoli <leonardo.mazzoli@studenti.unicam.it>
2022-05-25 11:42:03 +0200

    Progetto di base

---
+++ src/main/java/it/unicam/cs/ids/ingegneriadelsoftware/IngegneriadelsoftwareApplication.java
@@ -0,0 +1,14 @@
+package it.unicam.cs.ids.ingegneriadelsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class IngegneriadelsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngegneriadelsoftwareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/it/unicam/cs/ids/ingegneriadelsoftware/model/Roles.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.ids.ingegneriadelsoftware.model;
+
+public enum Roles {
+    USER,
+    ADMIN,
+    CICERONE
+}
