c06645ed1106a73d1e6e5bbcb19ad7bbd12ee3f0
LeonardoMazzoli <leonardo.mazzoli@studenti.unicam.it>
2022-06-24 10:24:42 +0200

    Added User Model

---
+++ src/main/java/it/unicam/cs/ids/ingegneriadelsoftware/model/User.java
@@ -0,0 +1,72 @@
+package it.unicam.cs.ids.ingegneriadelsoftware.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotEmpty
+    private String username;
+
+    @Column
+    @NotEmpty
+    private String password;
+
+    @Column
+    @NotEmpty
+    @Enumerated(EnumType.STRING)
+    private Roles roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(roles.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
