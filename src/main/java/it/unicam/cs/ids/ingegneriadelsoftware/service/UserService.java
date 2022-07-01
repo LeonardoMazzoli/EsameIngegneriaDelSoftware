07f37b9b306f9f9274cd688f8c0faa646c8393cc
gabrieleQuattrini <63640584+gabrieleQuattrini@users.noreply.github.com>
2022-07-01 09:18:18 +0200

    Update UserService

--- src/main/java/it/unicam/cs/ids/ingegneriadelsoftware/service/UserService.java
+++ src/main/java/it/unicam/cs/ids/ingegneriadelsoftware/service/UserService.java
@@ -14,5 +14,22 @@ import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.entityManager.createQuery("select u from User u where u.username =: username", User.class)
+                .setParameter("username", username).getSingleResult();
+    }
+
+    private User getUser(String username) {
+        return this.entityManager.createQuery("select u from User u where u.username =: username", User.class)
+                .setParameter("username", username).getSingleResult();
+    }
+
+    public Set<Trip> getUserTrips() {
+        User authentication = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return this.getUser(authentication.getUsername()).getUserTrips();
+    }
 }
